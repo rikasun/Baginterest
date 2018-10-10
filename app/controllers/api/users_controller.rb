e37d1e35feec28481484e596e8a2d97654e30718
Rika <ruijiasun1323@gmail.com>
2018-10-10 15:22:15 -0700

    Finish backend user auth

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,33 @@
+class Api::UsersController < ApplicationController
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      render "/api/users/show"
+      #should be home page all pins
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  # def show
+  #   @user = User.find(params[:id])
+  #   if @user
+  #     render :show
+  #   else
+  #     render json: @user.errors.full_messages, status: 404
+  #   end
+  # end
+
+  # def search
+  #   @users = User.where("username LIKE '%#{params[:query]}%'")
+  #   render json: @users
+  # end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :age, :password)
+  end
+end
